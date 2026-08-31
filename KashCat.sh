da0ebae0e2fb1bdd8f83acc58dbab49210917243
ZoeyTheArtGirl <sharkplayz1634@gmail.com>
2026-08-31 17:44:27 +0100

    $(id)

--- KashCat.sh
+++ KashCat.sh
@@ -1,10 +1,10 @@
-# Kash Cat
+# HashPaws2
 # Getting (root) by modifying /etc/shadow - Local Privilege Escalation 
 
 mkpasswd -m yescrypt "password123" # Boring example, but I used that.
 
 # Put this into /etc/shadow
-sudo sed -i 's|^root:[^:]*|root:|' /etc/shadow # Google creates a new string, even if same password, so paste your $y$ string
+sudo sed -i 's|^root:[^:]*|root:<string here>|' /etc/shadow # yescrypt creates a new string, even if same password, so paste your $y$ string
 
 # Log in, use your string
 su root
