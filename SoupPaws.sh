5d51f04b2c10675f44fff24778c9ed32f17a3c1a
ZoeyTheArtGirl <sharkplayz1634@gmail.com>
2026-09-09 14:24:08 +0100

    h

--- SoupPaws.sh
+++ SoupPaws.sh
@@ -1,4 +1,4 @@
-# Soup Paws - LPE via Arbitrary Additions to /etc/sudoers
+# Soup Paws - LPE via Arbitrary Additions to /etc/sudoers (Google Cloud Shell)
 
 # List all users, find your target
 compgen -u
