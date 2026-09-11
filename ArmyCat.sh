a002a5195359cac1a7a89bb21fc442e1422d9edd
ZoeyTheArtGirl <sharkplayz1634@gmail.com>
2026-09-11 09:35:54 +0100

    Shut up, Copilot.

---
+++ ArmyCat.sh
@@ -0,0 +1,4 @@
+# ArmyCat - another script editing LPE.
+
+# Surprisingly a one-liner, LOLFAO.
+sudo ucfr fpkg /tmp/fpkg.conf; . /usr/share/ucf/ucf_helper_functions.sh; PKGNAME=fpkg handle_deleted_ucf_file "/tmp/fpkg.conf" "/tmp/fpkgdir" "/tmp"
